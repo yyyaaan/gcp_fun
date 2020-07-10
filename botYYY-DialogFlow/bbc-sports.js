66ee1741e04a9684480fe41e2713b55e55b19744
Yan @ GCP <yan@yan.fi>
2020-07-10 09:17:53 +0100

    revert puppeteer version

--- botYYY-DialogFlow/bbc-sports.js
+++ botYYY-DialogFlow/bbc-sports.js
@@ -58,7 +58,7 @@ async function fetch_webpage(req_url){
     const browser = await puppeteer.launch({
         headless: true, 
 		ignoreHTTPSErrors: true,
-		args: ['--no-sandbox']
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
