faa939709aefed35201c4deb682a688e07960d3e
Yan @ GCP <yan@yan.fi>
2020-07-11 18:57:05 +0300

    forward request now handled in main()

--- botYYY-DialogFlow/index.js
+++ botYYY-DialogFlow/index.js
@@ -1,17 +1,17 @@
 // this is a webhook for DiaglogFlow; integration is handled there
-// https://googleapis.dev/nodejs/dialogflow/latest/index.html
 // https://github.com/dialogflow/dialogflow-fulfillment-nodejs/blob/master/src/dialogflow-fulfillment.js
 
 const {WebhookClient, Text, Card, Image, Suggestion, Payload} = require('dialogflow-fulfillment');
 const {agent_bqstatus, agent_lumo} = require('./query-bq.js');
-const {agent_schedule} = require('./forward-requst.js')
+const https = require("https");
+const testflag = "&test=yes"; // set non-BotYYY when call other function
 
 
 exports.main = (async (request, response) => {
     const agent = new WebhookClient({ request, response });
+    const line_replyToken = new String(request.body.originalDetectIntentRequest.payload.data.replyToken);
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-    const line_replyToken = new String(request.body.originalDetectIntentRequest.payload.data.replyToken);
 
     // hooks for different intentions
     function welcome(agent) {
@@ -25,9 +25,19 @@ exports.main = (async (request, response) => {
         agent.add(conv); // Add Actions on Google library responses to your agent's response
     }
 
+    async function agent_schedule(agent){
+        // call external with replyToken and possible date
+        var ext_url = "https://europe-west2-yyyaaannn.cloudfunctions.net/send-games" +
+                        "?replyToken=" + line_replyToken + testflag;
+        if(agent.parameters.date) {
+            ext_url = ext_url + "&date=" + agent.parameters.date.substring(0, 10)
+        }
+        https.get(ext_url, res => {console.log('fired external:' + ext_url)});
+    }
+
+
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
-
     intentMap.set('MyBQ Lumo', agent_lumo);
     intentMap.set('MyBQ Status', agent_bqstatus);
     intentMap.set('MyPP Sports', agent_schedule);
