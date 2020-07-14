3a0dc2e78bbb75fd3b7663faa4116a0878f01b3c
Yan @ GCP <yan@yan.fi>
2020-07-14 22:40:46 +0300

    optimize logging and isLine control

--- botYYY-DialogFlow/index.js
+++ botYYY-DialogFlow/index.js
@@ -1,12 +1,13 @@
 // this is a webhook for DiaglogFlow; integration is handled there
 // https://stackoverflow.com/questions/50163661/opensearch-call-in-dialogflow-webhook-intent-response-does-not-return-data
 
-
 const {WebhookClient, Text, Card, Image, Suggestion, Payload} = require('dialogflow-fulfillment');
 const {agent_bqstatus, agent_lumo} = require('./query-bq.js');
 const {agent_flightbq} = require('./flight-search.js');
 const https = require("https");
 const testflag = "&test=yes"; // set non-BotYYY when call other function
+var isLine = false; 
+
 
 function intentional_sleep(ms) {
   return new Promise((resolve) => {
@@ -15,17 +16,12 @@ function intentional_sleep(ms) {
 } 
 
 exports.main = (async (request, response) => {
-    const agent = new WebhookClient({ request, response });
-    if(request.body['originalDetectIntentRequest']){
-        const isLine = (request.body.originalDetectIntentRequest.source === 'line');
+    console.log(JSON.stringify({body: request.body, headers: request.headers}));
+    if(request.body.originalDetectIntentRequest){
+        isLine = (request.body.originalDetectIntentRequest.source === 'line');
+        console.log('Source LINE detected');
     } 
-    if(isLine){
-        const line_replyToken = new String(request.body.originalDetectIntentRequest.payload.data.replyToken);
-        const line_userId = new String(request.body.originalDetectIntentRequest.payload.data.source.userId);
-    }
-    // console.log(JSON.stringify(request.headers));
-    console.log(JSON.stringify(request.body));
-
+    const agent = new WebhookClient({ request, response });
 
     // hooks for different intentions
     function googleAssistantHandler(agent) { // not in use
@@ -38,6 +34,8 @@ exports.main = (async (request, response) => {
     async function agent_schedule(agent){
         // call external with replyToken and possible date
         if(isLine){
+            const line_replyToken = request.body.originalDetectIntentRequest.payload.data.replyToken;
+            // const line_userId = request.body.originalDetectIntentRequest.payload.data.source.userId;
             var ext_url = "https://europe-west2-yyyaaannn.cloudfunctions.net/send-games" +
                         "?replyToken=" + line_replyToken + testflag; //"?userId=" + line_userId
             if(agent.parameters.date) ext_url += "&date=" + agent.parameters.date.substring(0, 10);
@@ -48,7 +46,6 @@ exports.main = (async (request, response) => {
     }
 
 
-    // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('MyBQ Lumo', agent_lumo);
     intentMap.set('MyBQ Status', agent_bqstatus);
@@ -57,6 +54,3 @@ exports.main = (async (request, response) => {
     intentMap.set('Default Fallback Intent', googleAssistantHandler); // not in action
     agent.handleRequest(intentMap);
 });
-
-
-// (async ()=> {})();
