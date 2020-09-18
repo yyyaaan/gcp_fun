05204e7ef9d51069867567d7147b35dce22ba62e
Yan @ GCP <yan@yan.fi>
2020-09-18 21:19:50 +0000

    add status 200 response

--- all-scheduled/index.js
+++ all-scheduled/index.js
@@ -9,8 +9,10 @@ const schedule_lumo = [6,10,14];
 const schedule_hermes = [18];
 const schedule_skysports = [7];
 
-exports.main = (async(request, response) => {
+exports.main = (async(req, res) => {
     if(schedule_lumo.includes(cur_hour)) send_lumo(false);
     if(schedule_hermes.includes(cur_hour)) send_hermes();
     if(schedule_skysports.includes(cur_hour)) send_skysports();
+
+    res.status(200).send("completed");
 });
