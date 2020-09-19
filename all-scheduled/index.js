eec85bc5c25912505785b83973741cc816bd2864
Yan @ GCP <yan@yan.fi>
2020-09-19 15:25:38 +0000

    proper use of await

--- all-scheduled/index.js
+++ all-scheduled/index.js
@@ -5,14 +5,25 @@ const {send_skysports} = require('./src_skysports.js');
 
 // set proper schedule in UTC time zone
 const cur_hour = (new Date(Date.now())).getUTCHours();
-const schedule_lumo = [6,10,14];
+const schedule_lumo = [6,11,15];
 const schedule_hermes = [18];
 const schedule_skysports = [7];
 
 exports.main = (async(req, res) => {
-    if(schedule_lumo.includes(cur_hour)) send_lumo(false);
-    if(schedule_hermes.includes(cur_hour)) send_hermes();
-    if(schedule_skysports.includes(cur_hour)) send_skysports();
+    var info = `UTC${cur_hour}hr`;
+    if(schedule_lumo.includes(cur_hour)){
+        await send_lumo(false);
+        info += ' LUMO';
+    } 
+    if(schedule_hermes.includes(cur_hour)){
+        await send_hermes();
+        info += ' HERMES';
+    }
+    if(schedule_skysports.includes(cur_hour)){
+        await send_skysports();
+        info += ' SKYSPORTS';
+    } 
 
-    res.status(200).send("completed");
+    console.log(info);
+    res.status(200).send("completed " + info);
 });
